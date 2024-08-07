e286b102d581852c1775189ff1cc763852b9bc60
Tero F <erraticum@gmail.com>
2024-08-07 14:25:45 +0300

    Initial PhonebookBE

--- index.js
+++ index.js
@@ -1,115 +1,117 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:", request.path);
-  console.log("Body:", request.body);
-  console.log("---");
-  next();
-};
-app.use(requestLogger);
+morgan.token("tinyplus", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
-let notes = [
+let persons = [
   {
     id: "1",
-    content: "HTML is easy",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-]; // notes
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+]; // persons
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :tinyplus"
+  )
+);
 
 // routes
-app.get("/", (request, response) => {
-  response.send("<hi>Hello World!</h1>");
+app.get("/api/persons", (request, response) => {
+  response.send(persons);
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.get("/info", (request, response) => {
+  const time = Date();
+
+  response.send(`Phonebook has info for ${persons.length} people
+    <br><br>${Date()}`);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
+  const person = persons.find((p) => p.id === id);
 
-  if (note) {
-    response.json(note);
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
+
+  persons = persons.filter((p) => p.id !== id);
 
   response.status(204).end();
 });
 
 const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
+  return String(Math.floor(Math.random() * persons.length * 100000000));
 };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  //  console.log(body);
+
+  // no content sent with request
+  if (!body) {
     return response.status(400).json({
-      error: "content missing",
+      error: "missing content",
     });
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
+  // name or number missing
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "missing name or number",
+    });
+  }
+
+  // name already in the phonebook
+  const nameIdx = persons.findIndex((p) => p.name === body.name);
+  if (nameIdx !== -1) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
     id: generateId(),
+    name: body.name,
+    number: body.number,
   };
 
-  notes = notes.concat(note);
-
-  response.json(note);
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({
-    error: "unknown endpoint",
-  });
-}; // routes
-
-app.use(unknownEndpoint);
+  persons = persons.concat(person);
+  response.json(person);
+}); // routes
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
-
-// example code from https://fullstackopen.com/osa3/sovellus_internetiin
-// const PORT = process.env.PORT || 3001
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
-
-/*const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
-*/
