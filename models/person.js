63757026c8df2f8c5b6c668539cc60e75b99cfac
Tero F <erraticum@gmail.com>
2024-09-15 16:17:19 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("Connecting to", url);
+console.log('Connecting to', url)
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
-  });
+    console.log('Error connecting to MongoDB:', error)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -27,20 +27,20 @@ const personSchema = new mongoose.Schema({
     validate: {
       validator: function (numberGiven) {
         // ^[0-9]{2,3}-[0-9]{4,}$
-        return /^\d{2,3}-\d{4,}$/.test(numberGiven);
+        return /^\d{2,3}-\d{4,}$/.test(numberGiven)
       },
       message:
-        "Number must be 8 characters minimum, starting with 2-3 numbers, followed by '-', then rest of the numbers",
+        'Number must be 8 characters minimum, starting with 2-3 numbers, followed by \'-\', then rest of the numbers',
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
